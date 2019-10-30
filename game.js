04d53b35c9230a9e8ed9c84153fba50265fae5db
@douglasabnovato <douglasabnovato@gmail.com>
2019-10-30 12:02:12 -0300

    Local Json file

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-/* questions.json */
+/* Local JSON file : questions.json */
 let questions = [];
 
 fetch("questions.json") 

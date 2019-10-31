057d46e3e09bea852f46cb316630f815de9630a9
@douglasabnovato <douglasabnovato@gmail.com>
2019-10-31 12:09:22 -0300

    estrutura de pastas

--- src/javascript/end.js
+++ src/javascript/end.js
@@ -28,6 +28,6 @@ saveHighScore = e => {
     highScores.splice(8);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("/");
+    window.location.assign("../../");
 
 }
